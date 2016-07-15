f0138866d179efd019514d9c04a90828f8e37a69
ayaztalpur <mirayaz1@yahoo.com>
2016-07-15 17:46:37 +0500

    updated

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
+var DATA=[];
+
 $("#click").click(function(){
        var text = $('#myvalue').val()
-         $("#container").append('<div id="' + text + '"> <input type="checkbox" name="check" ><label>'+text+' </label><br></div> ');
-          $("#myvalue").val('');      
-          $("#"+text).click(function(){
-              $("#"+text).remove()
-              })
-        
-    
-    })
+       DATA.push(text);
+       $('#myvalue').val('');
+       RenderJSON(DATA);
+       save(DATA);
+
+});
+function RenderJSON(data){
+
+	$("#container").empty();
+	for (var i = 0; i < data.length; i++) {
+var text = $('#myvalue').val();
+         $("#container").append('<div id="' + data[i] + '"> <input type="checkbox" name="check" ><label>'+data[i]+' </label><br></div> ');
+$("#"+data[i]).click(function(){
+              var myid=$(this).attr("id");
+
+var index= DATA.indexOf(myid);
+              if (index >-1){
+                  DATA.splice(index,1);
+                  RenderJSON(DATA);
+                     save(DATA); 
+
+	}
+              });
+          }    
+    }
+    function save(){
+   localStorage.myData = JSON.stringify(DATA);
+}
+function load() {
+DATA = JSON.parse(localStorage.myData);
+RenderJSON(DATA);
+}
+
+load();
